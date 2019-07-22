8bc7f4d2a210767ae4bfba1c7904fe01fe54aaea
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-07-22 10:05:14 +0100

    Exercise 10.15 program terminated of JHTP 10ed.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_10/Exercises/Exercise_10_15/Invoice.java
@@ -0,0 +1,62 @@
+package books.JavaHowToProgram10Ed.chapter_10.Exercises.Exercise_10_15;
+
+public class Invoice implements Payable {
+
+    private final String partNumber;
+    private final String partDescription;
+    private int quantity;
+    private double pricePerItem;
+
+    public Invoice(String partNumber, String partDescription, int quantity,
+                   double pricePerItem) {
+        if (quantity < 0)
+            throw new IllegalArgumentException("Quantity must be >= 0");
+        if (pricePerItem < 0.0)
+            throw new IllegalArgumentException("Price per item must be >= 0");
+
+        this.quantity = quantity;
+        this.partNumber = partNumber;
+        this.partDescription = partDescription;
+        this.pricePerItem = pricePerItem;
+    }
+
+    public String getPartNumber() {
+        return this.partNumber;
+    }
+
+    public String getPartDescription() {
+        return this.partDescription;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity < 0)
+            throw new IllegalArgumentException("Quantity must be >= 0");
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setPricePerItem(double pricePerItem) {
+        if (pricePerItem < 0.0)
+            throw new IllegalArgumentException("Price per item must be >= 0");
+        this.pricePerItem = pricePerItem;
+    }
+
+    public double getPricePerItem() {
+        return this.pricePerItem;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %n%s: %s (%s) %n%s: %d %n%s: $%,.2f",
+                "invoice", "part number", this.getPartNumber(), this.getPartDescription(),
+                "quantity", this.getQuantity(), "price per item", this.getPricePerItem());
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return this.getQuantity() * this.getPricePerItem();
+    }
+}
---
+++ src/books/JavaHowToProgram10Ed/chapter_10/Exercises/Exercise_10_15/Payable.java
@@ -0,0 +1,6 @@
+package books.JavaHowToProgram10Ed.chapter_10.Exercises.Exercise_10_15;
+
+public interface Payable {
+
+    double getPaymentAmount();
+}
