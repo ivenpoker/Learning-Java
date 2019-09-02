449d883480a2cb79f745461fe41f524899240b90
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-03 00:52:21 +0100

    Added creation time to application
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/w3resource/basicPartOne/Exercise_1.java
+++ src/w3resource/basicPartOne/Exercise_1.java
@@ -4,7 +4,7 @@ package w3resource.basicPartOne;
 // #                                                                                      #
 // #    Program Purpose: Prints 'Hello' on screen and then print my name on separate line #
 // #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
-// #    Creation Date  :                                                                  #
+// #    Creation Date  : September 3, 2019                                                #
 // #                                                                                      #
 // ########################################################################################
 
