2553f70b81df891cd04e91b92863b9ab905ffe01
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-03 00:39:14 +0100

    Basic setup for the w3resource platform program exercises.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/App.java
@@ -0,0 +1,8 @@
+package w3resource;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Entry application for w3resource programs. Whooooh!!");
+    }
+}
