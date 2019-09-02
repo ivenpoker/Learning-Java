aaa54567210e91cde3718e50bddb541089bea2e4
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-03 00:46:29 +0100

    Program to print 'Hello' on screen and the print name on new line.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/basicPartOne/Exercise_1.java
@@ -0,0 +1,16 @@
+package w3resource.basicPartOne;
+
+// ########################################################################################
+// #                                                                                      #
+// #    Program Purpose: Prints 'Hello' on screen and then print my name on separate line #
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
+// #    Creation Date  :                                                                  #
+// #                                                                                      #
+// ########################################################################################
+
+public class Exercise_1 {
+
+    public static void main(String[] args) {
+        System.out.printf("%s\n%s", "Hello", "Happi Yvan");
+    }
+}
