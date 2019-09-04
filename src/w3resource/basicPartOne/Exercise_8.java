d8bcfc23cea2f845a355374cd5c53805dab37df2
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-04 02:08:05 +0100

    Program to print JAVA to console (terminal)
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/basicPartOne/Exercise_8.java
@@ -0,0 +1,21 @@
+package w3resource.basicPartOne;
+
+// ########################################################################################
+// #                                                                                      #
+// #    Program Purpose: Prints a little nice JAVA to console (terminal).                 #
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
+// #    Creation Date  : September 4, 2019                                                #
+// #                                                                                      #
+// ########################################################################################
+
+public class Exercise_8 {
+
+    public static void main(String[] args) {
+
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+
+    }
+}
