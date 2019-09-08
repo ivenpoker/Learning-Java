5ca5e7ce1905268d6e9b9f6b4fd2910d9b3bb848
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-08 23:06:40 +0100

    Program to compare two numbers.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/basicPartOne/Exercise_33.java
@@ -0,0 +1,56 @@
+package w3resource.basicPartOne;
+
+// ########################################################################################
+// #                                                                                      #
+// #    Program Purpose: Compares two numbers.                                            #
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
+// #    Creation Date  : September 8, 2019                                                #
+// #                                                                                      #
+// ########################################################################################
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise_33 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double firstNum = 0;
+        double secondNum = 0;
+        boolean valid = false;
+
+        do {
+            try {
+                System.out.print("Enter first number: ");
+                firstNum = input.nextInt();
+                valid = true;
+
+            } catch (InputMismatchException inputMismatchException) {
+                input.nextLine();           // clear input stream.
+                System.err.println("Invalid input. Please try again.");
+            }
+        } while (!valid);
+
+        valid = false;
+
+        do {
+            try {
+                System.out.print("Enter second number: ");
+                secondNum = input.nextInt();
+                valid = true;
+
+            } catch (InputMismatchException inputMismatchException) {
+                input.nextLine();           // clear input stream.
+                System.err.println("Invalid input. Please try again.");
+            }
+        } while (!valid);
+
+        if (firstNum < secondNum) {
+            System.out.printf("%.2f < %.2f\n", firstNum, secondNum);
+        } else if (firstNum > secondNum) {
+            System.out.printf("%.2f > %.2f\n", firstNum, secondNum);
+        } else {
+            System.out.printf("%.2f == %.2f\n", firstNum, secondNum);
+        }
+    }
+}
