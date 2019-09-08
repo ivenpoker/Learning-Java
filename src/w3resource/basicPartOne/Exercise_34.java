6c4e7d27b12b73b852ba88dab7f69d94f2408765
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-08 23:11:40 +0100

    Program to compute the sum of digits of an integer.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/basicPartOne/Exercise_34.java
@@ -0,0 +1,43 @@
+package w3resource.basicPartOne;
+
+// ########################################################################################
+// #                                                                                      #
+// #    Program Purpose: Computes the sum of digits of an integer.                        #
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
+// #    Creation Date  : September 8, 2019                                                #
+// #                                                                                      #
+// ########################################################################################
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise_34 {
+
+    private static int computeSumOfDigits(String someString) {
+        int sum = 0;
+        for (int i = 0; i < someString.length(); i++)
+            sum += Integer.parseInt("" + someString.charAt(i));
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int mainNumber = 0;
+        boolean valid = false;
+
+        do {
+            try {
+                System.out.print("Enter some integer: ");
+                mainNumber = input.nextInt();
+                valid = true;
+
+            } catch (InputMismatchException inputMismatchException) {
+                input.nextLine();           // clear input stream.
+                System.err.println("Invalid input. Please, try again.");
+            }
+        } while (!valid);
+
+        String mainString = Integer.toString(mainNumber);
+        System.out.printf("Sum of digits in %d: %d\n", mainNumber, computeSumOfDigits(mainString));
+    }
+}
