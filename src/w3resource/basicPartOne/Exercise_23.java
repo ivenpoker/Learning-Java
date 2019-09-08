0d1acb9ee36174745fe2ca0505823d9cd5be1b29
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-08 20:57:03 +0100

    Program to convert binary number to hexadecimal.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/basicPartOne/Exercise_23.java
@@ -0,0 +1,59 @@
+package w3resource.basicPartOne;
+
+// ########################################################################################
+// #                                                                                      #
+// #    Program Purpose: Converts binary number to hexadecimal.                           #
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
+// #    Creation Date  : September 8, 2019                                                #
+// #                                                                                      #
+// ########################################################################################
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise_23 {
+
+
+    private static int binaryToDecimal(int binaryNumber) {
+        int remainder = 0;
+        int decimalNumber = 0;
+        int j = 1;
+
+        while (binaryNumber != 0) {
+            remainder = binaryNumber % 10;
+            decimalNumber += remainder * j;
+            j *= 2;
+            binaryNumber /= 10;
+        }
+        return decimalNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int binaryNumber = 0;
+        boolean valid = false;
+
+        do {
+            try {
+                System.out.print("Enter binary number: ");
+                binaryNumber = input.nextInt();
+                valid = true;
+
+            } catch (InputMismatchException inputMismatchException) {
+                input.nextLine();       // clear input stream, discard remaining data.
+                System.err.println("Invalid input. Please, try again.");
+            }
+        } while (!valid);
+
+        // Assuming user number if binary....
+        try {
+            int decimalNumber = binaryToDecimal(binaryNumber);
+            String hexEquivalent = Integer.toHexString(decimalNumber);
+            System.out.printf("Hexadecimal equivalent: %s\n", hexEquivalent);
+
+        } catch (Exception exception) {
+            System.err.println("Error during conversion...\n");
+            exception.printStackTrace();
+        }
+    }
+}
