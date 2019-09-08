92a36d8e1ffca3bdff777d28b81ffe4666105011
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-08 17:46:24 +0100

    Program to add two binary numbers.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/basicPartOne/Exercise_16.java
@@ -0,0 +1,68 @@
+package w3resource.basicPartOne;
+
+// ########################################################################################
+// #                                                                                      #
+// #    Program Purpose: Add two binary numbers.                                          #
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
+// #    Creation Date  : September 4, 2019                                                #
+// #                                                                                      #
+// ########################################################################################
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise_16 {
+
+    public static void main(String[] args) {
+        long binary1 = 0;
+        long binary2 = 0;
+
+        int i = 0;
+        int remainder = 0;
+        int[] sum = new int[20];
+        boolean valid = false;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            try {
+                System.out.print("Input first binary number: ");
+                binary1 = input.nextLong();
+                valid = true;
+            } catch (InputMismatchException inputMismatch) {
+                input.nextLine();       // clear line
+                System.err.println("Invalid input. Please, try again.");
+            }
+        } while (!valid);
+
+        valid = false;
+
+        do {
+            try {
+                System.out.print("Input second binary number: ");
+                binary1 = input.nextLong();
+                valid = true;
+
+            } catch (InputMismatchException inputMismatch) {
+                input.nextLine();
+                System.err.println("Invalid input. Please, try again.");
+            }
+        } while (!valid);
+
+        while (binary1 != 0 || binary2 != 0) {
+            sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);
+            remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) / 2);
+            binary1 /= 10;
+            binary2 /= 10;
+        }
+        if (remainder != 0) {
+            sum[i++] = remainder;
+        }
+        --i;
+        System.out.print("Sum of two binary numbers: ");
+        while (i >= 0) {
+            System.out.print(sum[i--]);
+        }
+        System.out.println();
+    }
+
+}
