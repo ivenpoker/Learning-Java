9cd7ce810bbd8ffe1046ba57136f5781bc6e6a40
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-11 22:59:02 +0100

    Program to print a string in a specific format.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/basicPartOne/Exercise_43.java
@@ -0,0 +1,24 @@
+package w3resource.basicPartOne;
+
+// ########################################################################################
+// #                                                                                      #
+// #    Program Purpose: Print a string in a specific format.                             #
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
+// #    Creation Date  : September 11, 2019                                               #
+// #                                                                                      #
+// ########################################################################################
+
+public class Exercise_43 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Twinkle, twinkle, little star,");
+        System.out.println("\tHow I wonder what you are!");
+        System.out.println("\t\t\tUp above the world so high,");
+        System.out.println("\t\t\tLike a diamond in the sky.");
+        System.out.println("Twinkle, twinkle, little star,\t");
+        System.out.println("\t\tHow I wonder what you are.");
+
+    }
+
+}
