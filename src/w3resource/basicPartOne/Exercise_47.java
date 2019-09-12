82682c7c5b45838150164b464bb66df5e531770d
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-12 13:11:37 +0100

    Program to display current system time.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/basicPartOne/Exercise_47.java
@@ -0,0 +1,17 @@
+package w3resource.basicPartOne;
+
+// ########################################################################################
+// #                                                                                      #
+// #    Program Purpose: Display current system time.                                     #
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
+// #    Creation Date  : September 11, 2019                                               #
+// #                                                                                      #
+// ########################################################################################
+
+public class Exercise_47 {
+
+    public static void main(String[] args) {
+        System.out.printf("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+    }
+
+}
