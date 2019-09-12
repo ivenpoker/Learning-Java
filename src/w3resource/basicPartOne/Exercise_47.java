bae17d6fafc075c0331cc2991c7f448f8efca81e
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-12 13:11:55 +0100

    Date fix for previous commit.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/w3resource/basicPartOne/Exercise_47.java
+++ src/w3resource/basicPartOne/Exercise_47.java
@@ -4,7 +4,7 @@ package w3resource.basicPartOne;
 // #                                                                                      #
 // #    Program Purpose: Display current system time.                                     #
 // #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                            #
-// #    Creation Date  : September 11, 2019                                               #
+// #    Creation Date  : September 12, 2019                                               #
 // #                                                                                      #
 // ########################################################################################
 
