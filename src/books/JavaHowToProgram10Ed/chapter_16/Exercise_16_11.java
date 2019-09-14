4782ff370455e8a2f62b8ba7eeb18913e7562f69
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-14 15:35:53 +0100

    Solution to exercise 16.12 of chapter 16 of Java How to Program 10th edition.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercise_16_11.java
@@ -0,0 +1,25 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.12 from book.                      #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 14, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+import java.util.Properties;
+
+public class Exercise_16_11 {
+
+    public static void main(String[] args) {
+
+        /**
+         * load : Reads a property list (key and element pairs) from the input character stream in a simple line-oriented format.
+         * store: Writes the property list (key and element pairs) in the Properties table to the output character stream
+         *        in a format suitable for using the load(Reader) method
+         * getProperty: Searches for the property with the specified key in the property list.
+         * list: Prints the property list out to the specified output stream.
+         */
+    }
+}
