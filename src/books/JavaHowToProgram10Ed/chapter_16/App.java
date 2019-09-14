74bb190db0a9655538447d8f0cda0520de7357b7
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-14 13:50:25 +0100

    Start and solution to first exercise of Chapter 16
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_16/App.java
@@ -0,0 +1,4 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+public class App {
+}
---
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercise_16_3.java
@@ -0,0 +1,27 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.10 from book.                      #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 14, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_16_3 {
+
+    public static void main(String[] args) {
+        /**
+         * a) Collection : Interface that abstracts operations on a group of related objects.
+         * b) Collections: Class that provides static methods to operate on `Collection` objects.
+         * c) Comparator :  Interface that implemented by a class to say the class objects can be 'compared'.
+         * d) List       : A collection object that stores data of same type dynamically, can shrink and grow on execution.
+         * e) Load factor: That which is used to measure the state of hashing for object.
+         * f) Collision  : When two objects hash to same value.
+         * g) Space/Time : Whether to go with the advantages of space or that of time.
+         * h) HashMap    : Class that represents a data structure inherited from Map interface which uses hashing to store
+         *                 its data.
+         */
+
+    }
+}
