99f9778a8143c34b596b944f96e5db35d8353a88
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-14 14:28:53 +0100

    Solution to exercise 16.6 of chapter 16 of Java How to Program 10th edition.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercise_16_6.java
@@ -0,0 +1,22 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.4 from book.                       #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 14, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_16_6 {
+
+    public static void main(String[] args) {
+        /**
+         * A lot of methods inherited from the Vector class aren't really needed for the
+         * functionality of the Stack class. Also, since Stack inherits from Vector, nothing
+         * stops the user from calling vector-based-methods on stack, leaving the stack in
+         * an inconsistent state, which is not in correlation with the design and functionality
+         * of Stack data structure.
+         */
+    }
+}
