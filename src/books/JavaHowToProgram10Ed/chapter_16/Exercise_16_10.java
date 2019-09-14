0c65f468a6ce115b53a5ffca535eec33c5351058
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-14 15:30:53 +0100

    Solution to exercise 16.10 of chapter 16 of Java How to Program 10th edition.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercise_16_10.java
@@ -0,0 +1,22 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.10 from book.                      #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 14, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_16_10 {
+
+    public static void main(String[] args) {
+
+        /**
+         * a) Nope (false)
+         * b) False. Returns the LOWEST element in th TreeSet (which COULD be the first).
+         * c) False. List doesn't provide method(s) to resize itself.
+         */
+    }
+
+}
