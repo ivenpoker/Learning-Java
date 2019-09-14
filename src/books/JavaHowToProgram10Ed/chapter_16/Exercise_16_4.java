3b1018a13d612bc4fb1c200368bdeda632e9eab2
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-14 14:17:27 +0100

    Solution to exercise 16.4 of chapter 16 of Java How to Program 10th edition.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercise_16_4.java
@@ -0,0 +1,32 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.4 from book.                       #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 14, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+import java.util.Vector;
+
+public class Exercise_16_4 {
+
+    public static void main(String[] args) {
+        /**
+         * a) add: Adds an element to end of the vector
+         * b) set: Replaces the element at the specified position in the Vector with the specified element.
+         * c) remove: Removes the first occurrence of the specified element in the Vector If the Vector does
+         *            not contain the element, it is unchanged.
+         * d) removeAllElements: Removes all components from the vector and sets its size to zero.
+         * e) removeElementAt: Deletes the component at a specified index.
+         * f) firstElement: Returns the first component (the item at index 0) of the vector.
+         * g) lastElement: Returns the last component of the vector.
+         * h) contains: Returns true if this vector contains the specified element.
+         * i) indexOf: Returns the index of the first occurrence of the specified element in the vector, or -1 if the
+         *             vector does not contain the element.
+         * j) size: Returns the number of components in the vector.
+         * k) capacity: Returns the current capacity of the vector.
+         */
+    }
+}
