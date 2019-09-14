f70818b359c02335bd6079cf6adc431653d100af
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-14 15:24:18 +0100

    Solution to exercise 16.9 of chapter 16 of Java How to Program 10th edition.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercise_16_9.java
@@ -0,0 +1,23 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.4 from book.                       #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 14, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_16_9 {
+
+    public static void main(String[] args) {
+
+        /**
+         * a) put: Assigns a new value-object for a specified key-object.
+         * b) get: Get the value-object associated with a certain key-object.
+         * c) isEmpty: Determines if the HashMap is empty.
+         * d) containsKey: Determines if a specified key-object is found in HashMap.
+         * e) keySet: Returns a set of keys associated with a specified HashMap.
+         */
+    }
+}
