09c9a81548e97a922688039ed06eb53d1fe55e08
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-14 14:20:30 +0100

    Solution to exercise 16.5 of chapter 16 of Java How to Program 10th edition.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercise_16_5.java
@@ -0,0 +1,21 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.5 from book.                       #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 14, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_16_5 {
+
+    public static void main(String[] args) {
+        /**
+         * This is because inserting additional elements into a Vector object whose
+         * current size is less that its capacity, doesn't demand re-allocation of
+         * memory and copying the items into new memory locations, whereas in the
+         * other case there's need for re-allocation and copying of data.
+         */
+    }
+}
