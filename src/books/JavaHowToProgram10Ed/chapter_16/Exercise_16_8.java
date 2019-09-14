c30f8172928fdd68582b7292ba47ad6b8c5b1c56
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-14 15:19:06 +0100

    Solution to exercise 16.8 of chapter 16 of Java How to Program 10th edition.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercise_16_8.java
@@ -0,0 +1,21 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.8 from book.                       #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 14, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_16_8 {
+
+    public static void main(String[] args) {
+
+        /**
+         * a) iterator: Objects used to traverse a collection with a possibility of modifications
+         * b) hasNext : Determines if there are more objects in the iterator to iterate over.
+         * c) next    : Gets the current object under iteration
+         */
+    }
+}
