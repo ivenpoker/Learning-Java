9ae3d691d3d8c57d4bcc5bb8e596186b35427b30
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-15 22:16:16 +0100

    Solution to exercise 16.21 of chapter 16 of Java How to Program 10th edition.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercise_16_21.java
@@ -0,0 +1,32 @@
+package books.JavaHowToProgram10Ed.chapter_16;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.21 from book.                      #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 15, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Exercise_16_21 {
+
+    public static void main(String[] args) {
+
+        // queue of capacity 11
+        PriorityQueue<Double> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        // insert elements to queue
+        queue.offer(3.2);
+        queue.offer(9.8);
+        queue.offer(5.4);
+
+        System.out.print("Polling from queue: ");
+        while (queue.size() > 0) {
+            System.out.printf("%.1f ", queue.peek());
+            queue.poll();
+        }
+    }
+}
