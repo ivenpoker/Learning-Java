11051e80ab199694c0411b554bdcf0144dff1002
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-10-31 15:44:34 +0100

    Program to calculate the median of an given unsorted array of integers.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/w3resource/basicPartOne/Exercise_128.java
+++ src/w3resource/basicPartOne/Exercise_128.java
@@ -2,60 +2,46 @@ package w3resource.basicPartOne;
 
 // #######################################################################################################
 // #                                                                                                     #
-// #    Program Purpose: Computes the median of an given unsorted array of integers.                     #
+// #    Program Purpose: Compute the median of an given unsorted array of integers.                      #
 // #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                                           #
 // #    Creation Date  : October 30, 2019                                                                #
 // #                                                                                                     #
 // #######################################################################################################
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.LinkedList;
+import java.util.stream.IntStream;
+import java.security.SecureRandom;
 
 public class Exercise_128 {
 
-    private static class DataInfo {
-        public boolean found;
-        public int val;
-        public DataInfo(boolean found, int val) {
-            this.found = found;
-            this.val = val;
-        }
+    private static SecureRandom random = new SecureRandom();
+
+    private static int[] randomIntegerArray(int size, int low, int high) {
+        if (size < 0)
+            throw new IllegalArgumentException("Invalid size of array. Must be > 0");
+        int[] newArrayData = new int[size];
+        newArrayData = IntStream.of(newArrayData).map(val -> low  + random.nextInt(high)).toArray();
+        return newArrayData;
     }
 
-    private static DataInfo getSingleNumber(int[] mainData) {
+    private static double computeMedian(int[] mainData) {
+        if (mainData.length == 0) return -1;
 
-        boolean foundDouble = false;
-        List<Integer> tempList = new LinkedList<>();
-        DataInfo infoData = new DataInfo(false, Integer.MAX_VALUE);
+        //Arrays.sort(mainData);
+        int val = mainData.length / 2;
 
-        for (int i = 0; i < mainData.length; i++) {
-            if (tempList.contains(mainData[i]))
-                continue;
-            for (int j = i+1; j < mainData.length; j++) {
-                if (mainData[i] == mainData[j]) {
-                    foundDouble = true;
-                    tempList.add(mainData[i]);
-                    break;
-                }
-            }
-            if (!foundDouble || (i == mainData.length-1 && !tempList.contains(mainData[i]))) {
-                infoData.found = true;
-                infoData.val = mainData[i];
-                break;
-            }
+        if (mainData.length % 2 == 0) {
+            return (double) (mainData[val] + mainData[val+1]) / 2;
+        } else {
+            return mainData[val + 1];
         }
-        return infoData;
     }
 
     public static void main(String[] args) {
 
-        int[] tempData = {10, 20, 10, 20, 30, 40, 40, 30, 50};
+        int[] newArray = randomIntegerArray(10, 0, 20);
+        System.out.printf("Generated array data: %s\n", Arrays.toString(newArray));
+        System.out.printf("Computed mean is: %.1f\n", computeMedian(newArray));
 
-        System.out.printf("Main array data: %s\n", Arrays.toString(tempData));
-        DataInfo dataInfo = getSingleNumber(tempData);
-
-        System.out.printf("Number appearing only once: %s\n",
-                (dataInfo.found ? dataInfo.val : "[NONE]"));
     }
 }
