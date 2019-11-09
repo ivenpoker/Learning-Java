e086dbafed5e6291b7e5d87e01648e9c773c0dbe
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-11-09 20:24:01 +0100

    Program that finds possible unique paths considering some obstacles from top-left corner to bottom-right corner of a given grid (m x n).
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/basicPartOne/Exercise_137.java
@@ -0,0 +1,41 @@
+package w3resource.basicPartOne;
+
+// #######################################################################################################
+// #                                                                                                     #
+// #    Program Purpose: Find possible unique paths considering some obstacles, from top-left corner to
+// #                     bottom-right corner of a given grid (m x n).
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                                           #
+// #    Creation Date  : November 9, 2019                                                                #
+// #                                                                                                     #
+// #######################################################################################################
+
+public class Exercise_137 {
+
+    public static int uniquePaths_With_obstacle_Grid(int[][] obstacle_Grid) {
+        int m = obstacle_Grid.length;
+        if (m <= 0) {
+            return 0;
+        }
+        int n = obstacle_Grid[0].length;
+        if (n <= 0) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        dp[m][n - 1] = 1;
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                dp[i][j] = (obstacle_Grid[i][j] == 0) ? dp[i + 1][j] + dp[i][j + 1] : 0;
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        int[][] obstacle_Grid ={
+                {0, 0, 0},
+                {0, 1, 0},
+                {0, 0, 0},
+        };
+        System.out.println("Unique paths from top-left corner to bottom-right corner of the said grid (considering some obstacles): "+uniquePaths_With_obstacle_Grid(obstacle_Grid));
+    }
+}
