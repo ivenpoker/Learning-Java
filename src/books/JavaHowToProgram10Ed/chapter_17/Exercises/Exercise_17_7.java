77567b3733d13cce114b75168d42b9c9f29f08b5
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-01-12 21:30:05 +0100

    Exercise file that answers question 17.7 of Java How to Program 10thEd
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_17/Exercises/Exercise_17_7.java
@@ -0,0 +1,26 @@
+package books.JavaHowToProgram10Ed.chapter_17.Exercises;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 17.7 from book.                       #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : January 12, 2020                                           #
+// #                                                                                  #
+// ####################################################################################
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise_17_7 {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(new Integer[15]));
+
+    //  list.stream()                         // create a stream out of list items.
+    //         .filter(val -> val % 2 != 0)   // Use lambda to filter out all those values that fail predicate test
+    //         .sum();                        // sum all the items in the new filtered stream.
+
+    }
+}
