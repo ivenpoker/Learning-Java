eccb55e011cc37a179cceeb7f7c058d4076537ae
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-01-12 20:47:39 +0100

    Exercise file that answers question 17.4 of Java How to Program 10thEd
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_17/Exercises/Exercise_17_4.java
@@ -0,0 +1,30 @@
+package books.JavaHowToProgram10Ed.chapter_17.Exercises;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 17.4                                  #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : January 12, 2020                                           #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_17_4 {
+
+    public static void main(String[] args) {
+
+        // Responses to the question
+        //
+        // a) data -> Stream 'data' are formed from stream sources, intermediate operations and terminal operations
+        // b) External iteration.
+        // c) Immutability.
+        // d) Consumer<T>
+        // e) Supplier<T>
+        // f) stream of objects (even array of objects)
+        // g) filter
+        // h) collects
+        // i) stream operation
+        // j) compile
+        // k) abstract, default
+
+    }
+}
