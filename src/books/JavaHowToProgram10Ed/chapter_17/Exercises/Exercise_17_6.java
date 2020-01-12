a457e992f21cbc3e6a384378b8e2dc9a2dc1c679
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-01-12 21:25:08 +0100

    Exercise file that answers question 17.6 of Java How to Program 10thEd
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_17/Exercises/Exercise_17_6.java
@@ -0,0 +1,24 @@
+package books.JavaHowToProgram10Ed.chapter_17.Exercises;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 17.6 from book.                       #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : January 12, 2020                                           #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_17_6 {
+
+    public static void main(String[] args) {
+
+        // a) (double x, double y) -> {return x * y;}
+        // b) (x, y) -> {return x * y;}
+        // c) (x, y) -> x * y
+        // d) () -> "Welcome to lamdas!"
+        // e) ArrayList::new
+        // f) button.addActionListener((event) -> {
+        //        JOptionPane.showMessageDialog(ParentFrame.this, "JButton event Handler");
+        //    })
+    }
+}
