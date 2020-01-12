b784ce91053e8acc6724315c679ff6c56b993677
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-01-12 21:12:16 +0100

    Exercise file that answers question 17.5 of Java How to Program 10thEd
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_17/Exercises/Exercise_17_5.java
@@ -0,0 +1,27 @@
+package books.JavaHowToProgram10Ed.chapter_17.Exercises;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 16.15 from book.                      #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : September 15, 2019                                         #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_17_5 {
+
+    public static void main(String[] args) {
+
+        // Answer to the question in the book
+        //
+        // a) False. Intermediate operations are efficient because they're lazy in nature
+        //           won't be called until 'eager' operations are called.
+        // b) False. They don't always return a stream. 'filter' method will return a stream
+        //           but 'reduce' method won't. It returns a terminal value.
+        // c) False. The 'range' method second argument isn't inclusive, it's exclusive.
+        // d) True.
+        // e) True.
+        // f) True.
+        // g) True.
+    }
+}
