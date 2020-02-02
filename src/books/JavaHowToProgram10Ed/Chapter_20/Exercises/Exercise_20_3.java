0d4f27028104bed8c25a19ac1ce4a47878f2ae7a
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-02-02 23:08:49 +0100

    Solution to question 20.3 of book JHTP 10 Ed.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/Chapter_20/Exercises/Exercise_20_3.java
@@ -0,0 +1,22 @@
+package books.JavaHowToProgram10Ed.Chapter_20.Exercises;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 20.3 from book.                       #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : February 02, 2020                                          #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_20_3 {
+
+    public static void main(String[] args) {
+
+        // public class Array<T> {}
+        //
+        // Explanation: Code is definition for a generic class. ie
+        //              A class that accepts a type parameter 'T' is
+        //              defined.
+
+    }
+}
