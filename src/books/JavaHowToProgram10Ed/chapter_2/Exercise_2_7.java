384d712fcd480f5462018df82038d47159049511
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-04-15 22:49:05 +0100

    Solution to question 2.7 of book JHTP 10 Ed.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_2/Exercise_2_7.java
@@ -0,0 +1,24 @@
+package books.JavaHowToProgram10Ed.chapter_2;
+
+// ####################################################################################
+// #                                                                                  #
+// #      Program Purpose: Answers the question 2.7 from book.                        #
+// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
+// #      Program Date   : 15/04/2020                                                 #
+// #                                                                                  #
+// ####################################################################################
+
+public class Exercise_2_7 {
+
+    public static void main(String[] args) {
+
+        // Answer -> Comments
+        // Answer -> If
+        // Answer -> Assignment
+        // Answer -> % and /
+        // Answer -> innermost set
+        // Answer -> Variable
+
+    }
+
+}
