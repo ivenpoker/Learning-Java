1bea44ca89965863d28a290afddb7a77ca041c4e
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-04-25 23:15:16 +0100

    Documentation added for the solution of exercise 4.17 chapter 4 JHTP10Ed
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/books/JavaHowToProgram10Ed/chapter_4/Exercise_4_17.java
+++ src/books/JavaHowToProgram10Ed/chapter_4/Exercise_4_17.java
@@ -13,23 +13,36 @@ import java.util.Scanner;
 public class Exercise_4_17 {
 
     public static void main(String[] args) {
+
         double totalMilesPerGallon = 0.0;
         Scanner input = new Scanner(System.in);
 
+        // Request number of trips
         System.out.print("Enter number of trips: ");
         int tripsCnt = input.nextInt();
 
         System.out.println();
+
+        // For each trip
         while (tripsCnt > 0) {
+
+            // Obtain miles driven from user
             System.out.printf("[%d] Enter miles driven: ", tripsCnt);
             int milesDriven = input.nextInt();
 
+            // Obtains gallon(s) used from user
             System.out.printf("[%d] Enter gallons used: ", tripsCnt);
             int gallonsUsed = input.nextInt();
+
+            // Display miles per gallon value.
             System.out.printf("Miles per gallon: %.2f\n\n", (double) milesDriven / gallonsUsed);
+
+            // Update the total miles per gallon data
             totalMilesPerGallon += (double) milesDriven / gallonsUsed;
             --tripsCnt;
         }
+
+        // Alert the user of the total miles per gallon used.
         System.out.printf("Total miles per gallon: %.2f\n", totalMilesPerGallon);
     }
 }
