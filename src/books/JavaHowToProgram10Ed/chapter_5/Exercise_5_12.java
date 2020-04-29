c4570dd7553eed2f51ea5f592a3fc7b23d80a076
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-04-29 11:55:37 +0100

    JHTP10ed: Bug fix for Exercise 5.12
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/books/JavaHowToProgram10Ed/chapter_5/Exercise_5_12.java
+++ src/books/JavaHowToProgram10Ed/chapter_5/Exercise_5_12.java
@@ -12,7 +12,7 @@ public class Exercise_5_12 {
 
     public static void main(String[] args) {
         int ans = 1;
-        for (int i = 1; i < 15; i += 2)
+        for (int i = 1; i <= 15; i += 2)
             ans *= i;
         System.out.printf("Product is: %d\n", ans);
     }
