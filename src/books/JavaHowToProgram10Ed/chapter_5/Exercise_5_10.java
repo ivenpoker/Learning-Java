ed3421da49d955bdd41b2127cc27a6c1d8fed285
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-04-29 11:42:44 +0100

    Refactor Exercise 5.10 answer in JHTP10ed.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/books/JavaHowToProgram10Ed/chapter_5/Exercise_5_10.java
+++ src/books/JavaHowToProgram10Ed/chapter_5/Exercise_5_10.java
@@ -11,12 +11,15 @@ package books.JavaHowToProgram10Ed.chapter_5;
 public class Exercise_5_10 {
 
     public static void main(String[] args) {
+
         // Code will print 5 '@'s on 10 lines (each 5 '@'s on a single line).
 
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 5; j++)
+        for (int i = 1; i <= 10; i++) {         // Repeat statements in body 10 times.
+
+            for (int j = 1; j <= 5; j++)        // Repeat statement in body 5 times for every single parent iteration.
                 System.out.print('@');
-            System.out.println();
+
+            System.out.println();               // Before moving to the next parent iteration print a new line.
         }
     }
 }
