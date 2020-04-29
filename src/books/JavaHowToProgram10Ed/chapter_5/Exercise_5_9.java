4436b2861918821c081a294af93c7e50d8d09234
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-04-29 11:36:59 +0100

    Refactor Exercise 5.9 answer in JHTP10ed.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/books/JavaHowToProgram10Ed/chapter_5/Exercise_5_9.java
+++ src/books/JavaHowToProgram10Ed/chapter_5/Exercise_5_9.java
@@ -11,14 +11,20 @@ package books.JavaHowToProgram10Ed.chapter_5;
 public class Exercise_5_9 {
 
     public static void main(String[] args) {
-        // ANS a): The 'For' has capital letter 'F' in head. It should be lowercase.
-        // ANS b): The problem is that the 'case's don't have the 'break' keyword.
-        //         As a result, whether the value of 'value % 2 is 0 or 1, it'll
-        //         always print 'Even integer' then 'Odd integer'. To fix this,
-        //         we need to put a break keyword at the end of the 'case's.
-        // ANS c): The problem is the 'for loop' is going to iterate infinitely.
-        //         FIX: change the 'i += 2 ' to 'i -= 2'
-        // ANS d): do-while is to print till 100, but will print till 98, to fix it
-        //         let the condition in the 'while' statement be 'counter <= 100'
+        // ANS a): - The 'For' has capital letter 'F' in head. It should be lowercase.
+        //         - The variable 'i' should be decrementing instead of incrementing. In this case
+        //           it will cause an infinite-loop when execution reaches the statement.
+
+        // ANS b): - The error here is that, the outcome of 'value % 2' is either 0 and 1,
+        //           and case labels in the switch statement doesn't have to a 'break' statement
+        //           for each case. As a result, if the value of 'value % 2' is 0, execution will fall through
+        //           'case 0' and 'case 1'.
+
+        // ANS c): - The problem is that, the 'for loop' is going to iterate infinitely.
+        //           FIX: change the 'i += 2 ' to 'i -= 2', so we should be decrementing by 2 instead of
+        //           incrementing by 2.
+
+        // ANS d): - do-while loop, is to print till 100, but will print till 98. To fix it,
+        //           let the condition in the 'while' statement be 'counter <= 100'.
     }
 }
