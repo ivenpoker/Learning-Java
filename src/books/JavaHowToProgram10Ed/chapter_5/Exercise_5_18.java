52577ccbdf13804970533231864f54155aaa25c7
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-04-29 19:07:24 +0100

    Just add not finished solution to Exercise 5.18
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_5/Exercise_5_18.java
@@ -0,0 +1,21 @@
+package books.JavaHowToProgram10Ed.chapter_5;
+
+public class Exercise_5_18 {
+
+    public static void main(String[] args) {
+
+        double amount;
+        double principal = 1000.0;
+        double rate = 0.05;
+
+        // Display headers
+        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+
+        for (int year = 1; year <= 10; ++year) {
+            amount = principal * Math.pow(1.0 + rate, year);
+            System.out.printf("%4d%,20.2f%n", year, amount);
+        }
+
+    }
+
+}
