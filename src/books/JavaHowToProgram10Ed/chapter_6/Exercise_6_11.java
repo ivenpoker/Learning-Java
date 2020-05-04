f3d0bf0328b120da2e9a30e58cb8940ed08b540f
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-05-04 10:17:53 +0100

    JHTP10ed: Slight modification on Exercise_6_11 solution.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/books/JavaHowToProgram10Ed/chapter_6/Exercise_6_11.java
+++ src/books/JavaHowToProgram10Ed/chapter_6/Exercise_6_11.java
@@ -14,14 +14,17 @@ public class Exercise_6_11 {
         // ANS:
         //
         // a) It means to choose a number based on some unpredictable pattern, in
-        //    which, how the number was obtained cannot be figured out.
+        //    which ... the means, manner, pattern or algorithm used to obtained the the number cannot be figured out.
+        //    In other words, reverse engineering isn't possible.
         //
-        // b) This is because 'game of chance' is based on integers from the dice.
+        // b) This is because 'game of chance' is based generating random numbers within a specified range.
+        //    And the range of numbers on which the 'game of chance' operates, is obtainable via 'nextInt' method.
         //    Hence the method is just perfect for this purpose.
         //
         // c) To obtain random data within a desired range.
         // d) This helps us (Programmers) to assert that our algorithms is working
         //    as desired, since the data which is obtained through random generation
-        //    is not predefined by us.
+        //    is not predefined by us. In a nutshell, it is ideal for testing purposes and to find
+        //    bugs.
     }
 }
