90d8eb5fa49aaf8113c385e4acf9429240401f63
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-05-15 10:56:33 +0100

    JHTP10ed: Solution to Exercise 7.6 of Chapter 7
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_7/Exercise_7_6/Exercise_7_6.java
@@ -0,0 +1,16 @@
+package books.JavaHowToProgram10Ed.chapter_7.Exercise_7_6;
+
+public class Exercise_7_6 {
+
+    public static void main(String[] args) {
+
+        // Answer to questions:
+        //
+        // a) Elements are, p[0], p[1], p[2], p[3]
+        // b) Declaring the array
+        // c) row, column.
+        // d) m, n, m x n.
+        // e) d[2][4]
+    }
+
+}
