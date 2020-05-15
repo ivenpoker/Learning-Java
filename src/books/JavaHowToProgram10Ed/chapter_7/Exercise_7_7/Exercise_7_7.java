7cdc7ed88c812e6196e7ad133c7b3145f5486787
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-05-15 11:07:12 +0100

    JHTP10ed: Solution to Exercise 7.7 of Chapter 7
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/books/JavaHowToProgram10Ed/chapter_7/Exercise_7_7/Exercise_7_7.java
@@ -0,0 +1,28 @@
+package books.JavaHowToProgram10Ed.chapter_7.Exercise_7_7;
+
+public class Exercise_7_7 {
+
+    public static void main(String[] args) {
+
+        // Answer to the questions:
+        //
+        // Question a): False, we specify the name of the array and the index at which
+        //              the particular element occurs, not the 'value' of the element.
+        //
+        // Question b): False. Declaration only creates reference to be used to process
+        //              data at specified location (not yet created) by programmer. Array
+        //              instantiation, 'reserves' the space for the for array.
+        //
+        // Question c): False, one will rather write:
+        //
+        //              int[] p = new int[100];
+        //
+        // Question d): Not necessarily. The application can use 'Arrays.fill(nameOfArray, 0)'
+        //              to initialize the contents of the array.
+        //
+        // Question e): Yes, it must contain (this is true for what is covered so far in the
+        //              chapter, other techniques like functional programming wouldn't need two
+        //              for loops)
+
+    }
+}
