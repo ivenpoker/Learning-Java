0d544f9524791e31e8c5e37b6acbe589fdcb3a78
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-08-09 13:50:21 +0100

    [JHTP10ed]: Moved Exercise 16.12 to its own package and with an alternative implementation.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/books/JavaHowToProgram10Ed/chapter_16/Exercises/Exercise_16_12/AlternativeImplementation.java
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercises/Exercise_16_12/AlternativeImplementation.java
@@ -1,54 +1,58 @@
-package books.JavaHowToProgram10Ed.chapter_16.Exercises;
-
-// ####################################################################################
-// #                                                                                  #
-// #      Program Purpose: Answers the question 16.12 from book.                      #
-// #      Program Author : Happi Yvan <ivensteinpoker@gmail.com>                      #
-// #      Program Date   : September 14, 2019                                         #
-// #                                                                                  #
-// ####################################################################################
+package books.JavaHowToProgram10Ed.chapter_16.Exercises.Exercise_16_12;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
-public class Exercise_16_12 {
+public class AlternativeImplementation {
 
+    // output list contents
     private static void printList(List<String> list) {
         System.out.printf("%nlist:%n");
-        for (String color: list)
+
+        for (String color : list) {
             System.out.printf("%s ", color);
+        }
         System.out.println();
     }
 
+    // locate String objects and convert to uppercase
     private static void convertToUppercaseStrings(List<String> list) {
         ListIterator<String> iterator = list.listIterator();
+
         while (iterator.hasNext()) {
             String color = iterator.next();
             iterator.set(color.toUpperCase());
         }
     }
 
+    // Obtain sublist and use clear method to delete sublist items
     private static void removeItems(List<String> list, int start, int end) {
         list.subList(start, end).clear();
     }
 
-    private static void printReverseList(List<String> list) {
+    private static void printReversedList(List<String> list) {
         ListIterator<String> iterator = list.listIterator(list.size());
-        System.out.printf("%nReversed List: %n");
+
+        System.out.printf("%nReversed List:%n");
 
         // print list in reverse order
-        while (iterator.hasPrevious())
+        while (iterator.hasPrevious()) {
             System.out.printf("%s ", iterator.previous());
+        }
     }
 
     public static void main(String[] args) {
-        String[] colors = {"black", "yellow", "green", "blue", "violet", "silver"};
+
+        // add colors elements to list1
+        String[] colors =
+                {"black", "yellow", "green", "blue", "violet","silver"};
         List<String> list1 = new LinkedList<>(Arrays.asList(colors));
 
-        // Add color1 elements to list2
-        String[] colors2 = {"gold", "white", "brown", "blue", "gray", "silver"};
+        // add colors2 elements to list2
+        String[] colors2 =
+                {"gold", "white", "brown", "blue", "gray", "silver"};
         List<String> list2 = new LinkedList<>(Arrays.asList(colors2));
 
         list1.addAll(list2);
@@ -58,12 +62,11 @@ public class Exercise_16_12 {
         convertToUppercaseStrings(list1);
         printList(list1);
 
-        System.out.printf("%nDeleting element 4 to 6....");
+        System.out.printf("%nDeleting elements 4 to 6 ...");
         removeItems(list1, 4, 7);
         printList(list1);
-        printReverseList(list1);
+        printReversedList(list1);
 
     }
-
 
 }
