fa8bf515e84319a9dfb3bf422ab963f44f5cf4af
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-08-09 12:49:43 +0100

    [JHTP10ed]: Moved Exercise 16.7 to its own package and some slight code changes done.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/books/JavaHowToProgram10Ed/chapter_16/Exercises/Exercise_16_7/Exercise_16_7.java
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercises/Exercise_16_7/Exercise_16_7.java
@@ -1,4 +1,4 @@
-package books.JavaHowToProgram10Ed.chapter_16.Exercises;
+package books.JavaHowToProgram10Ed.chapter_16.Exercises.Exercise_16_7;
 
 // ####################################################################################
 // #                                                                                  #
@@ -14,8 +14,9 @@ public class Exercise_16_7 {
 
     /**
      * a) A set stores unique single elements (objects) whereas a map stores both
-     *    a key (unique) and a value (both are objects).
-     * b) Auto-boxing occurs, converting the primitive type to an object type wrapper.
+     *    a key (unique) and a value (both are objects). That is, 'Map' do map keys
+     *    to values, whereas 'Sets' contains only values (unique values).
+     * b) Auto-boxing occurs, converting the primitive type to an object type wrapper (Double).
      * c) Yes, using enhance for loop.
      */
 }
