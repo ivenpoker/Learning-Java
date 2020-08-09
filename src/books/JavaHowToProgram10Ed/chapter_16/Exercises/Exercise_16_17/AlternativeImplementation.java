7ade42e7711e213b7fd4040a30714fdc05468fb9
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-08-09 22:45:26 +0100

    [JHTP10Ed] Removed redundant import from previous program.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- src/books/JavaHowToProgram10Ed/chapter_16/Exercises/Exercise_16_17/AlternativeImplementation.java
+++ src/books/JavaHowToProgram10Ed/chapter_16/Exercises/Exercise_16_17/AlternativeImplementation.java
@@ -4,7 +4,6 @@ import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class AlternativeImplementation {
 
