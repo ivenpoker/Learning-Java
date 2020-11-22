a5dc7530c52e7572bd1ef7c535f454ab6be29e18
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-11-22 13:11:16 +0100

    [W3R]: Program that compares a given string to the specified character sequence.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ src/w3resource/utils/Exercise_9.java
@@ -0,0 +1,23 @@
+package w3resource.utils;
+
+// ###########################################################################################
+// #                                                                                         #
+// #    Program Purpose: Compares a given string to the specified character sequence.        #
+// #    Program Author : Happi Yvan <ivensteinpoker@gmail.com>                               #
+// #    Creation Date  : November 22, 2020                                                   #
+// #                                                                                         #
+// ###########################################################################################
+
+public class Exercise_9 {
+
+    public static void main(String[] args) {
+        String str1 = "example.com";
+        String str2 = "Example.com";
+
+        CharSequence mainCharSeq = "example.com";
+
+        System.out.printf("Comparing [A] and [B]: %s%n", str1.contentEquals(mainCharSeq));
+        System.out.printf("Comparing [B] and [A]: %s%n", str2.contentEquals(mainCharSeq));
+    }
+
+}
